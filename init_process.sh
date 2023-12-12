4ab73e006d79e74ab7ddc55fa37416238200c11f
Fabio Torres <fabio.torres@utec.edu.pe>
2023-12-12 07:27:13 -0300

    feat: added bash script to run process

---
+++ init_process.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+pid=$(ps aux | grep controlRobot | grep -v grep | awk '{print $2}')
+
+if [ -z "$pid" ]; then
+    echo "--------"
+    echo $(date)
+    echo "Starting Control Robot"
+    cd /home/caninos/projeto
+    screen -dmS controlRobot -L -Logfile /home/caninos/logs/rebot.log python3 -m robcontrol -s --log-level INFO
+fi
