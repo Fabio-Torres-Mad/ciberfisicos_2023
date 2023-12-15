b4550397e5c3237c6087499681207f4d757558f0
Fabio Torres <fabio.torres@utec.edu.pe>
2023-12-15 03:02:52 -0300

    refact: set gpio

--- robcontrol/drivers/servo.py
+++ robcontrol/drivers/servo.py
@@ -1,42 +1,40 @@
 import time
 import logging
+import os
 import caninos_sdk as k9
-
+# from robcontrol.libs.pwm import PW
 
 LOG = logging.getLogger(__name__)
 
 class ServoMotor:
-    def __init__(self, conf: ServoConfig) -> None:
+    def __init__(self) -> None:
         LOG.info(f"Init Servo driver")
-        self.conf: conf
         self.labrador = k9.Labrador()
-        self.labrador.pin(conf.control).enable_gpio(
-            k9.Pin.Direction.OUTPUT
-        )
-
-    def right(self) -> None:
-        self.labrador.pin(self.conf.control).enable_pwm(
-            freq=50,
-            duty_cycle=0.025
-        )
-        self.labrador.pin(self.conf.control).pwm.start()
-        sleep(0.5)
-        self.labrador.pin(self.conf.control).pwm.stop()
-
-    def center(self) -> None:
-        self.labrador.pin(self.conf.control).enable_pwm(
-            freq=50,
-            duty_cycle=0.075
-        )
-        self.labrador.pin(self.conf.control).pwm.start()
-        sleep(0.5)
-        self.labrador.pin(self.conf.control).pwm.stop()
 
     def left(self) -> None:
-        self.labrador.pin(self.conf.control).enable_pwm(
-            freq=50,
-            duty_cycle=0.12
-        )
-        self.labrador.pin(self.conf.control).pwm.start()
-        sleep(0.5)
-        self.labrador.pin(self.conf.control).pwm.stop()
+        LOG.info("Servo to left")
+        self.labrador.pin33.enable_pwm(alias="servo", freq=50, duty_cycle=0.12)
+        LOG.debug("Start left")
+        self.labrador.servo.pwm.start()
+        time.sleep(0.5)
+        LOG.debug("Stop left")
+        self.labrador.servo.pwm.stop()
+
+
+    def center(self) -> None:
+        LOG.info("Servo to center")
+        self.labrador.pin33.enable_pwm(alias="servo", freq=50, duty_cycle=0.07)
+        LOG.debug("Start center")
+        self.labrador.servo.pwm.start()
+        time.sleep(0.4)
+        LOG.debug("Stop center")
+        self.labrador.servo.pwm.stop()
+
+    def right(self) -> None:
+        LOG.info("Servo to right")
+        self.labrador.pin33.enable_pwm(alias="servo", freq=50, duty_cycle=0.02)
+        LOG.debug("Start left")
+        self.labrador.servo.pwm.start()
+        time.sleep(0.5)
+        LOG.debug("Stop left")
+        self.labrador.servo.pwm.stop()
